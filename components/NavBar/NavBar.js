768d55e4e373fac74a2999c11e08649703278008
Shane <31248360+shanedle@users.noreply.github.com>
2022-03-06 22:57:15 +0100

    removed unnecessary code :fire:

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,18 +1,8 @@
-import { useColorMode, Button, Flex, Box } from "@chakra-ui/react";
-import NextLink from "next/link";
-import { useRouter } from "next/router";
+import { Flex } from "@chakra-ui/react";
 import Logo from "./Logo";
 import ThemeToggleButton from "./ThemeToggleButton";
 
 const Navbar = () => {
-  const { colorMode } = useColorMode();
-  const router = useRouter();
-
-  const navHoverBg = {
-    light: "gray.100",
-    dark: "gray.700",
-  };
-
   return (
     <Flex
       flexDirection="row"
@@ -23,50 +13,6 @@ const Navbar = () => {
       mx="auto"
     >
       <Logo />
-      <Box>
-        <NextLink href="/blog" passHref>
-          <Button
-            as="a"
-            variant="ghost"
-            p={[1, 2, 4]}
-            _hover={{ backgroundColor: navHoverBg[colorMode] }}
-            backgroundColor={
-              router.pathname.includes("/blog") ? navHoverBg[colorMode] : null
-            }
-            aria-label="Blog"
-          >
-            Blog
-          </Button>
-        </NextLink>
-        <NextLink href="/projects" passHref>
-          <Button
-            as="a"
-            variant="ghost"
-            p={[1, 2, 4]}
-            _hover={{ backgroundColor: navHoverBg[colorMode] }}
-            backgroundColor={
-              router.pathname === "/projects" ? navHoverBg[colorMode] : null
-            }
-            aria-label="Projects"
-          >
-            Projects
-          </Button>
-        </NextLink>
-        <NextLink href="/" passHref>
-          <Button
-            as="a"
-            variant="ghost"
-            p={[1, 2, 4]}
-            _hover={{ backgroundColor: navHoverBg[colorMode] }}
-            backgroundColor={
-              router.pathname === "/" ? navHoverBg[colorMode] : null
-            }
-            aria-label="Home"
-          >
-            Home
-          </Button>
-        </NextLink>
-      </Box>
       <ThemeToggleButton />
     </Flex>
   );
